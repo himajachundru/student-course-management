6a7fa8a2edf48204ea61f1c03b37ba37dafe488d
himajachundru <himajachundru1603@gmail.com>
2026-08-18 03:08:53 -0700

    Allow React frontend CORS

--- backend/student-management/src/main/java/com/example/student_management/controller/StudentController.java
+++ backend/student-management/src/main/java/com/example/student_management/controller/StudentController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/students")
-@CrossOrigin(origins = "http://localhost:5174")
+@CrossOrigin(origins = "http://localhost:5173")
 public class StudentController {
 
     private final StudentService studentService;
